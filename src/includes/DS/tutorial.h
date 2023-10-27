a0afbfc8299375c755de83e31e790bc71b0295e7
Deryck Schnee <145064390+deryckschnee-dev@users.noreply.github.com>
2023-10-26 18:01:00 -0700

    comment formatting

--- src/includes/DS/tutorial.h
+++ src/includes/DS/tutorial.h
@@ -10,23 +10,25 @@
 /* #####    FUNCTION DECLARATIONS                       #####
 */
 
-/* ds_Test: add two integers, return sum. 
-*/  int ds_Test(int firstNumber, int secondNumber);
+/* ds_Test: add two integers, return sum. */
+    int ds_Test(int firstNumber, int secondNumber);
 
-/* ds_TestTwo: multiply two integers, return product. 
-*/  int ds_TestTwo(int firstNumber, int secondNumber);
+/* ds_TestTwo: multiply two integers, return product. */  
+    int ds_TestTwo(int firstNumber, int secondNumber);
 
 /* ds_ConvertTempToC: takes Fahrenheit, perform
-    conversion, return Celsius.
-*/  int ds_ConvertTempToC(int);
+    conversion, return Celsius. */
+    int ds_ConvertTempToC(int);
 
 /* ds_GetLine: read a line into specified array, 
     add NULL '\0' termination character,
-    return length of array.
-*/  int ds_GetLine(char *lineArray, int maxLine);
+    return length of array. */
+    int ds_GetLine(char *lineArray, int maxLine);
 
-/* ds_CopyLine: copy source array to destination array.
-*/  void ds_CopyLine(char *destination, char *source);
+/* ds_CopyLine: copy source array to destination array. */
+    void ds_CopyLine(char *destination, char *source);
 
-/* ds_AppendLine: append source array to destination array.
-*/  void ds_AppendLine(char *destination, char *source, int offsetIndex, int maxSize);
+/* ds_AppendLine: append source array to destination array. 
+    NOTE: append offset is not determined within this function 
+    and must be provided. */  
+    void ds_AppendLine(char *destination, char *source, int offsetIndex, int maxSize);
